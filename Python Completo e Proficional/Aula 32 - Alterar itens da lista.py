93a6773295ba964717c2379fd9dacaf0a305d7d2
Mauri Sebastiao da Luz <iruam_mauri@msn.com>
2022-05-23 15:47:59 -0300

    Alterar itens da lista

---
+++ Python Completo e Proficional/Aula 32 - Alterar itens da lista.py
@@ -0,0 +1,22 @@
+
+# Alterar itens da lista
+nome = ["Gabriel", "Dany", "Artur", "João"]
+#nome[1] = "Danielle"
+#print(nome)
+
+# Alterar mais de um item da lista
+#nome[1:3] = ["Glória", "Beatriz"]
+#print(nome)
+
+
+# Alterar e add item alem do especificado no indice
+#nome[1:3] = ["Glória", "Beatriz", "Flavio", "Lucas"]
+#print(nome)
+
+# colocar um item ja existente na lista
+#nome[1:3] = ["Lucas"]
+#print(nome)
+
+# Inserir(Add) item na lista
+#nome.insert(2, "Lucas")
+#print(nome)
