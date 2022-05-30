d7a55bdbd385213b60f321cc6632c1fa8068999d
Mauri Sebastiao da Luz <iruam_mauri@msn.com>
2022-05-30 13:45:44 -0300

    Desafio 78 feito

--- Python Completo e Proficional/Exercicios/listas/Desafio 78 lista.py
+++ Python Completo e Proficional/Exercicios/listas/Desafio 78 lista.py
@@ -1,3 +1,36 @@
+import os
+os.system("clear") or None
 
 # Faça um programa que leia 5 valores numericos e guarde-os em uma lista.
-# No final, mostre qual foi o maior valor digitado e as suas respectivas posiçoes no lista. 
+# No final, mostre qual foi o maior valor digitado e as suas respectivas posiçoes no lista.
+
+lista = []
+maiorValor = 0
+menorValor = 0
+
+for c in range(0, 5):
+    lista.append(int(input("Digite um valor para a Posição {}: ".format(c))))
+    if c ==0:
+        maiorValor = menorValor = lista[c]
+    else:
+        if lista[c] > maiorValor:
+            maiorValor = lista[c]
+        if lista[c] < menorValor:
+            menorValor = lista[c]
+
+
+
+
+print('=-' * 22)
+print("Voçe digitou os valores {}".format(lista))
+
+print("O maior valor digitado foi {} na posiçao".format(maiorValor), end='')
+for i, v in enumerate(lista):
+    if v == maiorValor:
+        print(". {} ...".format(i))
+
+
+print("O menor valor digitado foi {} na posiçao".format(menorValor), end='')
+for i, v in enumerate(lista):
+    if v == menorValor:
+        print(". {} ...".format(i))
